8e8b8f4fdd50b1ab35abae609675c7fdecfab3f6
Casper Chen <casperchen0325@gmail.com>
2026-03-22 16:06:47 +0800

    updated about me page

--- src/Desktop.jsx
+++ src/Desktop.jsx
@@ -2,24 +2,26 @@ import { useState } from "react"
 import "./App.css"
 import AppIcon from "./components/AppIcon.jsx"
 import Page from "./components/Page.jsx"
-import ToolBar from "./components/ToolBar.jsx"
-import MessageRoller from "./components/MessageRoller.jsx"
+import ToolBar from "./components/ToolBar.jsx" 
 
 import com from "./assets/com.png"
 import networks from "./assets/networks.png"
 import projects from "./assets/bin.png"
+import journal from "./assets/journal.png"
 
 function Desktop() {
   const icons = [
     { id: 1, name: "About Me", src: com},
     { id: 2, name: "Connects", src: networks},
-    { id: 3, name: "Projects", src: projects}
+    { id: 3, name: "Projects", src: projects},
+    { id: 4, name: "Journal", src: journal}
   ]
 
   const pages = [
     {id: 1, tag: "About Me", size: {width: "600px", height: "400px"}, icon: icons[0]},
     {id: 2, tag: "Connects", size: {width: "800px", height: "500px"}, icon: icons[1]},
-    {id: 3, tag: "Projects", size: {width: "800px", height: "500px"}, icon: icons[2]}
+    {id: 3, tag: "Projects", size: {width: "800px", height: "500px"}, icon: icons[2]},
+    {id: 4, tag: "Journal", size: {width: "800px", height: "500px"}, icon: icons[3]}
   ]
 
   const [selectedIcon, setSelectedIcon] = useState(null);
@@ -34,7 +36,7 @@ function Desktop() {
   return <div 
     className="desktop"
     onClick={() => setSelectedIcon(null)}>
-      <span style={{position: "absolute", right: "10px"}}>(c) 2026 mry. All Rights Reserved.</span>
+      <span style={{position: "absolute", right: "10px"}}> 2026 mry. All Rights Reserved.</span>
     {icons.map(icon => (
         <AppIcon 
           key={icon.id}
--- src/components/PageContent.jsx
+++ src/components/PageContent.jsx
@@ -4,6 +4,11 @@ import game from "../assets/game.gif"
 import disk from "../assets/disk.gif"
 import cat from "../assets/cat.gif"
 import avatar from "../assets/avatar.png"
+import info from "../assets/info.png"
+
+import free from "../assets/free.gif"
+import ms from "../assets/microsoft.gif"
+import win95 from "../assets/win95.gif"
 
 function WiggleText({amp, time, texts, font, color}) {
   return <span>
@@ -27,70 +32,87 @@ function WiggleText({amp, time, texts, font, color}) {
 function PageContent({key, tag}) {
   if(tag == "About Me")
   return <div className="page-content">
-    <div style={{textAlign: "center"}}>
+    <div style={{textAlign: "center", color: "rgb(0, 0, 122)"}}>
       <h1>WELCOME TO MRY.DEV!</h1>
       <br />
-      <WiggleText amp={1} time="0.8" texts="*Message of the Day*" font="Times"/>
+      <WiggleText amp={1} time="0.8" texts="*Message of the Day*" font="Times" color="black" />
     </div>
     <MessageRoller />
     <br />
     <br />
     <div style={{display: "flex", alignItems: "center"}}>
-      <img src={avatar} alt="com" 
+      <img src={avatar} 
+        alt="avatar" 
+        id="avatar"
         style={{display: "inline-block",
         position: "relative",
-        top: "-15px",
         width: "200px", 
         animation: "rotate 2.5s ease-in-out infinite"
       }}/>
-      <div>
-        Hi, I'm mry, right now I'm a sophomore student majoring in CS, who only codes when he feels like it.
-        <br />
-        <br />
-        I really like to play video games, and one of my dreams is to make one my own.
-        <br />
-        <br />
+      <div style={{display: "flex", flexDirection: "column"}}>
+          <div className="tab" style={{maxHeight: "20px", margin: "0px", maxWidth: "130px"}}>
+            <img src={info} alt="info" style={{width: "30px", objectFit: "contain"}} />
+            Introduction
+          </div>
+        <div className="win-panel">
+          Hi, I'm mry, right now I'm a sophomore student majoring in CS, who only codes when he feels like it.
+          <br />
+          <br />
+          I really like to play video games, and one of my dreams is to make one my own.
+          <br />
+          <br />
+        </div>
       </div>
     </div>
+    <br />
     <div className="title"><img src={game} alt="game" style={{width: "60px"}} /> Games that I like:</div>
-    <ul>
-      <li>Minecraft</li>
-      <li>Undertale</li>
-      <li>Omori</li>
-      <li>Stardew Valley</li>
-      <li>Elden Ring</li>
-      <li>And a lot more...</li>
-    </ul> 
+    <div className="win-panel" style={{padding: "0px"}}>
+      <ul>
+        <li>Minecraft</li>
+        <li>Undertale</li>
+        <li>Omori</li>
+        <li>Stardew Valley</li>
+        <li>Elden Ring</li>
+        <li>And a lot more...</li>
+      </ul> 
+    </div>
     <br />
     I enjoy anime and music a lot too, though i recently stop watching as much as before :(
     <br />
     <br />
     <div className="title"><img src={tv} alt="tv" style={{width: "60px"}} /> Animes I like:</div>
-    <ul>
-      <li>Chainsaw man</li>
-      <li>JJK</li>
-      <li>CPK</li>
-      <li>Death Note</li>
-      <li>Kimetsu no Yaiba</li>
-    </ul>
+    <div className="win-panel" style={{padding: "0px"}}>
+      <ul>
+        <li>Chainsaw man</li>
+        <li>JJK</li>
+        <li>CPK</li>
+        <li>Death Note</li>
+        <li>Kimetsu no Yaiba</li>
+      </ul>
+    </div>
     <br />
     <div className="title"><img src={disk} alt="disk" style={{width: "60px"}} />And here are some facts about me:</div>
-    <ul>
-      <li>I'm from Taiwan.</li>
-      <li>My favorite drink is {<WiggleText amp={1} time={1} texts="black milk tea" font="TimesB" color="#57392a"/>}! (no sugar)</li>
-      <li>I could play very very little guitar. (trying my best rn)</li>
-      <li>And I also like to play basketball.</li>
-    </ul>
+    <div className="win-panel" style={{padding: "0px"}}>
+      <ul>
+        <li>I'm from Taiwan.</li>
+        <li>My favorite drink is {<WiggleText amp={1} time={1} texts="black milk tea" font="TimesB" color="#57392a"/>}! (no sugar)</li>
+        <li>I could play very very little guitar. (trying my best rn)</li>
+        <li>And I also like to play basketball.</li>
+      </ul>
+    </div>
     <br />
     <div className="box">
       <img src={cat} alt="cat" style={{width: "200px"}}/>
-      <div>
-        <span style={{fontFamily: "TimesB"}}>“If it ain't broke, don't fix it” </span>
+      <div style={{marginLeft: "20px"}}>
+        <span style={{fontFamily: "TimesB"}}>“This website is made ugly intentionally, which is perfect since I have no artistic talent whatsoever” </span>
         <br />
-        <span style={{fontFamily: "TimesB"}}>--presumably my cat</span>
-        <br />
-        <span style={{fontFamily: "TimesB"}}>--on the left is not my cat</span>
+        <span style={{fontFamily: "TimesB"}}>--proudly by me</span>
       </div>
+    </div>
+    <div style={{display: "flex", justifyContent: "right", gap: "5px"}}>
+      <img src={win95} alt="" />
+      <img src={ms} alt="" />
+      <img src={free} alt="" />
     </div>
   </div>
 }
